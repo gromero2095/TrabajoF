ccb9f382a4282fe852eedfd9ad8febde0dce2096
gromero2095 <gersonromero2095@gmail.com>
2017-05-20 04:04:27 -0500

    Actualizacion 4am

---
+++ TrabajoF/TrabajoF.Entities/IRepositories/IAutorRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoF.Entities.IRepositories
+{
+    interface IAutorRepository
+    {
+    }
+}
